6763a572bfa15ac76ee6ba1511a1b17869cce32c
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2020-07-20 11:43:42 -0400

    Getting an error when program starts
    InitializeSystem: Sig Number 1155 out of range Specified for
    IP-ID-30.Slot-1000

--- ControlSystem.cs
+++ ControlSystem.cs
@@ -29,14 +29,27 @@ namespace NvxSspTesting
             try
             {
                 _tx = new DmNvx350(0x30, this);
+                _tx.OnlineStatusChange += nvx_OnlineStatusChange;
                 _tx.Register();
 
                 _rx = new DmNvx350(0x31, this);
+                _rx.OnlineStatusChange += nvx_OnlineStatusChange;
                 _rx.Register();
             }
             catch (Exception e)
             {
                 ErrorLog.Error("Error in InitializeSystem: {0}", e.Message);
+                ErrorLog.Error(e.StackTrace);
+            }
+        }
+
+        private void nvx_OnlineStatusChange(GenericBase dev, OnlineOfflineEventArgs args)
+        {
+            if (args.DeviceOnLine)
+            {
+                var nvxDevice = dev as DmNvx350;
+
+                CrestronConsole.PrintLine("{0} is ONLINE", nvxDevice.Name);
             }
         }
     }
