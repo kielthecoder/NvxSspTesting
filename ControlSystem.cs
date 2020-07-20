963da93aa1d86cf53b6bc39e0baead8e970cb6a2
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2020-07-20 11:25:54 -0400

    Initial check in
    Minimal program that all it does is build without errors.

---
+++ ControlSystem.cs
@@ -0,0 +1,35 @@
+using System;
+using Crestron.SimplSharp;
+using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.CrestronThread;
+
+namespace NvxSspTesting
+{
+    public class ControlSystem : CrestronControlSystem
+    {
+        public ControlSystem()
+            : base()
+        {
+            try
+            {
+                Thread.MaxNumberOfUserThreads = 25;
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Error("Error in the constructor: {0}", e.Message);
+            }
+        }
+
+        public override void InitializeSystem()
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Error("Error in InitializeSystem: {0}", e.Message);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("NvxSspTesting")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NvxSspTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
